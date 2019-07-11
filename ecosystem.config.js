fcc314835c83407f466252bf11b8b6ea234a183e
Brett Hannigan <bchannig@sfu.ca>
2019-07-11 15:47:39 -0700

    Changed PM2 config again...

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     production: {
       user: 'ubuntu',
       host: 'ec2-35-174-111-99.compute-1.amazonaws.com',
-      key: 'C:/Users/bchannig/.ssh/ensc-menrva-19.pem"',
+      key: 'C:/Users/bchannig/.ssh/ensc-menrva-19.pem',
       ref: 'origin/master',
       repo: 'git@github.com:bretthannigan/e-man.git',
       path: '/home/ubuntu/e-man',
