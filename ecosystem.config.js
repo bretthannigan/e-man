2d85dbdfd8f885a656f926049941d3a05d5d94cf
Brett Hannigan <bchannig@sfu.ca>
2019-07-11 15:54:11 -0700

    Trying to fix PM2.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       key: 'C:/Users/bchannig/.ssh/ensc-menrva-19.pem',
       ref: 'origin/master',
       repo: 'git@github.com:bretthannigan/e-man.git',
-      path: '/home/ubuntu/e-man',
+      path: './',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
