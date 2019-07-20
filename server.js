3173137b80c1918c35157cdc31396fa596aa14ab
Brett Hannigan <bchannig@sfu.ca>
2019-07-19 17:04:47 -0700

    Date added/modified/due added to web app.

--- server.js
+++ server.js
@@ -199,49 +199,25 @@ app.delete('/delete/:id', function (req, res) {
 app.post('/new', function(req, res) {
     req.body.date_added = new Date(Date.now()).toISOString();
     req.body.date_modified = new Date(Date.now()).toISOString();
-    dbMain.collection(ASSET_DB).save(req.body, function(err, result) {
-        console.log(req.body);
+    dbMain.collection(ASSET_DB).findOneAndUpdate({asset_number: { $eq: req.body.asset_number } }, { $set: req.body }, { asset_number: 1, upsert: true}, function(err, result) {
         if (err) throw err;
-        console.log('saved to database');
+        console.log('saved to database.')
         res.redirect('/dashboard');
     })
+    // dbMain.collection(ASSET_DB).save(req.body, function(err, result) {
+    //     console.log(req.body);
+    //     if (err) throw err;
+    //     console.log('saved to database');
+    //     res.redirect('/dashboard');
+    // })
 });
-
-// // This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
-// app.get('/oauth', function(req, res) {
-//     console.log('oauth')
-//     // When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
-//     if (!req.query.code) {
-//         res.status(500);
-//         res.send({"Error": "Looks like we're not getting a code."});
-//         console.log("Looks like we're not getting a code.");
-//     } else {
-//         // If it's there...
-//         // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
-//         console.log(req)
-//         request({
-//             url: 'https://slack.com/api/oauth.access', //URL to hit
-//             qs: {code: req.query.code, client_id: process.env.CLIENTID, client_secret: process.env.CLIENTSECRET}, //Query string data
-//             method: 'GET', //Specify the method
-//         }, function (error, response, body) {
-//             var JSONresponse = JSON.parse(body);
-//             if (error) {
-//                 console.log(error);
-//             } else if (!JSONresponse.ok) {
-//                 console.log(JSONresponse)
-//                 res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end();
-//             } else {
-//                 console.log(JSONresponse);
-//                 res.json(body);
-//             }
-//         })
-//     }
-// });
 
 //----------Slash Commands----------
 
 var verifySlackRequest = function (req, res, next) {
     const hmac = crypto.createHmac('sha256', process.env.SIGNING_SECRET);
+    console.log(req.headers)
+    console.log(req)
     var slack_signature = req.headers['x-slack-signature'];
     var signature = slack_signature.split('=', 1);
     version = signature[0];
