de97ee5e3bae933e3ddbdce73c45c0de09c49bac
Brett Hannigan <hanniganbrett@gmail.com>
2021-02-25 09:50:17 -0800

    Change EC2 public DNS

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,12 +6,12 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-52-25-111-176.us-west-2.compute.amazonaws.com',
+      host: 'ec2-54-191-135-95.us-west-2.compute.amazonaws.com',
       key: '/Users/brett/.ssh/BrettsMacPro.pem',
       ref: 'origin/master',
       repo: 'git@github.com:bretthannigan/e-man.git',
       path: '/home/ubuntu/e-man',
-      'pre-deploy-local': 'scp -i /Users/brett/.ssh/BrettsMacPro.pem .env ubuntu@ec2-52-25-111-176.us-west-2.compute.amazonaws.com:/home/ubuntu/.env',
+      'pre-deploy-local': 'scp -i /Users/brett/.ssh/BrettsMacPro.pem .env ubuntu@ec2-54-191-135-95.us-west-2.compute.amazonaws.com:/home/ubuntu/.env',
       'pre-deploy': 'git reset --hard',
       'post-deploy': 'npm install && cp ~/.env .env && pm2 startOrRestart ecosystem.config.js'
     }
